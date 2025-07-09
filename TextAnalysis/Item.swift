4b00342f31627a979bcef0726ecae4f29da520ae
Harry <Harryyan1238@gmail.com>
2025-07-09 18:50:54 +1200

    Initial Commit

---
+++ TextAnalysis/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TextAnalysis
+//
+//  Created by HarryYan on 09/07/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TextAnalysisTests/TextAnalysisTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextAnalysisTests.swift
+//  TextAnalysisTests
+//
+//  Created by HarryYan on 09/07/2025.
+//
+
+import Testing
+@testable import TextAnalysis
+
+struct TextAnalysisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
